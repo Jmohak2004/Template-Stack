3b630e3fc478eebe7c88901f6942c4f47e8d45ad
MOHAK P JAISWAL <145755209+Jmohak2004@users.noreply.github.com>
2025-06-26 13:44:07 +0000

    tailwind and shadcn done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import { Button } from "@/components/ui/button"
+
+
 const App = () => {
   return (
     <div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
       <Routes>
         <Route to='/form'><Form/></Route>
         <Route to='/dashboard'><Dashboard/></Route>
