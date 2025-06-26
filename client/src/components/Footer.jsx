68d9df293a277b43bbffa099a18790cd3331995b
MOHAK P JAISWAL <145755209+Jmohak2004@users.noreply.github.com>
2025-06-26 13:37:18 +0000

    components added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      This is a basic form-put-dashboard-fetch app made for freshers in the internship
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to='/dashboard'>Form</Link>
+      <Link to='/form'>Dashboard</Link>
+    </div>
+  )
+}
+
+export default Navbar
